cf90066b9e25e57b20d877f4b2839dc4d71e10a9
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-16 16:42:30 +0300

    -Stocks endpoint interface

---
+++ app/src/main/java/com/mergenc/bigmarket/data/remote/StockApi.kt
@@ -0,0 +1,17 @@
+package com.mergenc.bigmarket.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getList(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "YHTOQGJVB03QLVUC"
+        const val BASE_URL = "https://alphavantage.co"
+    }
+}
