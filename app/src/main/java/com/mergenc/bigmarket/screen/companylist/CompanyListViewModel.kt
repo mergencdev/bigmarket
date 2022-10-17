6ce0c034281338c8321d885cfda95c8fdb007b6d
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-17 16:59:30 +0300

    -Get company list by cases

--- app/src/main/java/com/mergenc/bigmarket/screen/companylist/CompanyListViewModel.kt
+++ app/src/main/java/com/mergenc/bigmarket/screen/companylist/CompanyListViewModel.kt
@@ -2,25 +2,63 @@ package com.mergenc.bigmarket.screen.companylist
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mergenc.bigmarket.domain.repository.StockRepository
+import com.mergenc.bigmarket.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CompanyListViewModel @Inject constructor(
     private val repository: StockRepository
 ) : ViewModel() {
-    val state by mutableStateOf(CompanyListState())
+    var state by mutableStateOf(CompanyListState())
+    private var searchJob: Job? = null
 
     fun onEvent(event: CompanyListEvent) {
         when (event) {
             is CompanyListEvent.Refresh -> {
-
+                getCompanyList(fetchFromRemote = true)
             }
             is CompanyListEvent.OnSearchQueryChange -> {
-
+                state = state.copy(searchQuery = event.query)
+                searchJob?.cancel()
+                searchJob = viewModelScope.launch {
+                    delay(500L)
+                    getCompanyList()
+                }
             }
+        }
+    }
+
+    fun getCompanyList(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ) {
+        viewModelScope.launch {
+            repository.getCompanyList(fetchFromRemote, query)
+                .collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            result.data?.let { companies ->
+                                state = state.copy(
+                                    companies = companies
+                                )
+                            }
+                        }
+                        is Resource.Error -> {
+
+                        }
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                    }
+                }
         }
     }
 }
