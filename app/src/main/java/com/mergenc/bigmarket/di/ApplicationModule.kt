68676b60b88e507e43d9121a2287214aa2880f2d
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-19 21:19:17 +0300

    -Create app module with retrofit

---
+++ app/src/main/java/com/mergenc/bigmarket/di/ApplicationModule.kt
@@ -0,0 +1,38 @@
+package com.mergenc.bigmarket.di
+
+import android.app.Application
+import androidx.room.Room
+import com.mergenc.bigmarket.data.local.StockDatabase
+import com.mergenc.bigmarket.data.remote.StockApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideStockDatabase(app: Application): StockDatabase {
+        return Room.databaseBuilder(
+            app,
+            StockDatabase::class.java,
+            "stockdb.db"
+        ).build()
+    }
+}
