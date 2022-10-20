3e91eafdbe0969a5a33eb16ba3bcc0154d05c8f0
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-20 19:32:43 +0300

    -Repository module for bindings

---
+++ app/src/main/java/com/mergenc/bigmarket/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.mergenc.bigmarket.di
+
+import com.mergenc.bigmarket.data.csv.CSVParser
+import com.mergenc.bigmarket.data.csv.CompanyListParser
+import com.mergenc.bigmarket.data.repository.StockRepositoryImpl
+import com.mergenc.bigmarket.domain.model.CompanyList
+import com.mergenc.bigmarket.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListParser(
+        companyListParser: CompanyListParser
+    ): CSVParser<CompanyList>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+
+}
