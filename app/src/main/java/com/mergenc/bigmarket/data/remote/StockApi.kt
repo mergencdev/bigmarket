63031fa3c156f442f8efcc74fe083647190dfa75
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-21 17:34:12 +0300

    -GET requests for intraday and company info

--- app/src/main/java/com/mergenc/bigmarket/data/remote/StockApi.kt
+++ app/src/main/java/com/mergenc/bigmarket/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.mergenc.bigmarket.data.remote
 
+import com.mergenc.bigmarket.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,6 +10,18 @@ interface StockApi {
     suspend fun getList(
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntraday(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): CompanyInfoDto
 
     companion object {
         const val API_KEY = "YHTOQGJVB03QLVUC"
