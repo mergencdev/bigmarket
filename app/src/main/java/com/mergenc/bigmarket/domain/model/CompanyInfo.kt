c977b8b60eba60567cab2281d8fc4a815ee9bdc1
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-21 17:32:02 +0300

    -Company info model

---
+++ app/src/main/java/com/mergenc/bigmarket/domain/model/CompanyInfo.kt
@@ -0,0 +1,9 @@
+package com.mergenc.bigmarket.domain.model
+
+data class CompanyInfo(
+    val name: String,
+    val symbol: String,
+    val description: String,
+    val country: String,
+    val industry: String
+)
