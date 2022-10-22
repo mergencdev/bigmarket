7e48f402eaa5eed0f14059b1d04dbe83289e77f7
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-22 16:47:51 +0300

    -Change theme for login screen & add colors

--- app/src/main/java/com/mergenc/bigmarket/ui/theme/Theme.kt
+++ app/src/main/java/com/mergenc/bigmarket/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.mergenc.bigmarket.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
@@ -13,11 +14,36 @@ private val DarkColorPalette = darkColors(
     onBackground = Color.White
 )
 
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
 @Composable
 fun BigMarketTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
 
     MaterialTheme(
         colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun LoginScreenUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
