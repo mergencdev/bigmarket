b94772bb8707f64b7bca39472eb6e3a3458e1623
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-23 01:44:13 +0300

    -Log-out when on back pressed in Company list screen

--- app/src/main/java/com/mergenc/bigmarket/screen/companylist/CompanyListScreen.kt
+++ app/src/main/java/com/mergenc/bigmarket/screen/companylist/CompanyListScreen.kt
@@ -1,5 +1,6 @@
 package com.mergenc.bigmarket.screen.companylist
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,31 +9,42 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
+import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.huawei.agconnect.auth.AGConnectAuth
 import com.mergenc.bigmarket.domain.model.CompanyList
 import com.mergenc.bigmarket.screen.companyinfo.CompanyInfoScreen
 import com.mergenc.bigmarket.screen.destinations.CompanyInfoScreenDestination
+import com.mergenc.bigmarket.screen.destinations.LoginScreenDestination
+import com.mergenc.bigmarket.ui.theme.bigMarketOrange
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
+@Destination
 fun CompanyListScreen(
     navigator: DestinationsNavigator,
-    navController: NavController,
     viewModel: CompanyListViewModel = hiltViewModel()
 ) {
     val refreshState = rememberSwipeRefreshState(isRefreshing = viewModel.state.isRefreshing)
     val state = viewModel.state
 
+    // Log-out when on back pressed;
+    BackHandler {
+        AGConnectAuth.getInstance().signOut()
+        navigator.navigate(LoginScreenDestination)
+    }
+    
     // Search column;
     Column(
         modifier = Modifier.fillMaxSize()
