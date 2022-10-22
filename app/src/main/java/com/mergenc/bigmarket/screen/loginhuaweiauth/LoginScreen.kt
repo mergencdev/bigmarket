8c0a3b99cff3270ac4ef34ea1784b29ef7bccafa
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-10-23 01:45:15 +0300

    -Login with credential, e-mail (HUAWEI Auth services) -Custom toast function

--- app/src/main/java/com/mergenc/bigmarket/screen/loginhuaweiauth/LoginScreen.kt
+++ app/src/main/java/com/mergenc/bigmarket/screen/loginhuaweiauth/LoginScreen.kt
@@ -1,23 +1,36 @@
 package com.mergenc.bigmarket.screen.loginhuaweiauth
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.huawei.agconnect.auth.AGConnectAuth
+import com.huawei.agconnect.auth.EmailAuthProvider
 import com.mergenc.bigmarket.common.*
+import com.mergenc.bigmarket.screen.destinations.CompanyListScreenDestination
+import com.mergenc.bigmarket.screen.destinations.RegisterScreenDestination
+import com.mergenc.bigmarket.screen.registerhuaweiauth.RegisterScreen
 import com.mergenc.bigmarket.ui.theme.PinkColor
 import com.mergenc.bigmarket.ui.theme.bigMarketGray
+import com.mergenc.bigmarket.ui.theme.bigMarketOrange
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
 @Destination(start = true)
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(
+    navController: NavController,
+    navigator: DestinationsNavigator
+) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     Surface(
@@ -65,9 +78,30 @@ fun LoginScreen(navController: NavController) {
 
             Spacer(modifier = Modifier.height(24.dp))
 
+            val toastContext = LocalContext.current
+
             CommonLoginButton(text = "Login", modifier = Modifier.fillMaxWidth()) {
                 Log.d("LoginScreen", "Login button clicked")
+
+                if (email.isNotBlank() && password.isNotBlank()) {
+
+                    Log.d("login info", "email: $email, password: $password")
+                    val credential =
+                        EmailAuthProvider.credentialWithPassword(email, password)
+
+                    AGConnectAuth.getInstance().signIn(credential).addOnSuccessListener {
+                        makeToast(toastContext, "Login successful")
+                        navigator.navigate(CompanyListScreenDestination)
+                    }.addOnFailureListener {
+                        makeToast(toastContext, "Login failed")
+                    }
+                }
             }
+
+            /*CommonLoginButton(text = "log-out", modifier = Modifier.fillMaxWidth()) {
+                AGConnectAuth.getInstance().signOut()
+                makeToast(toastContext, "Logged out")
+            }*/
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -85,9 +119,21 @@ fun LoginScreen(navController: NavController) {
                     fontSize = 18.sp,
                     fontWeight = FontWeight.W500
                 ) {
-                    navController.navigate("register_screen")
+                    navigator.navigate(RegisterScreenDestination)
                 }
             }
         }
     }
+}
+
+private fun makeToast(context: Context, msg: String) {
+    Toast.makeText(context, "$msg", Toast.LENGTH_LONG).show()
+}
+
+@Composable
+fun CircularProgressIndicator() {
+    CircularProgressIndicator(
+        color = bigMarketOrange,
+        strokeWidth = 4.dp
+    )
 }
